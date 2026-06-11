199d91e23066c30aa9891bdea8c0d2782009ef21
Yasin <yasinhassan004@gamil.com>
2026-06-11 13:24:59 +0300

    Add Loan constuctor with basic attributes

--- MODELS/loan.py
+++ MODELS/loan.py
@@ -1,3 +1,13 @@
 from datetime import datetime, timedelta
 class Loan:
     loan_counter = 0
+
+    def __init__(self, book_id, borrower_id, due_date):
+        self.id = Loan.loan_counter
+        Loan.loan_counter += 1
+
+        self.book_id = book_id
+        self.borrower_id = borrower_id
+        self.due_date = due_date
+        self.returned = False
+        
