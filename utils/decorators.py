91ee70a487969480ca37840d1767bba455e59c05
Pipsy <gpflaslik7112@gmail.com>
2026-06-12 13:09:23 +0300

    chore: remove unused auth/security/decorators files

--- utils/decorators.py
+++
@@ -1,5 +0,0 @@
-def require_login(func):
-    def wrapper(*args, **kwargs):
-        print("Checking authentication...")
-        return func(*args, **kwargs)
-    return wrapper
--- utils/security.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
