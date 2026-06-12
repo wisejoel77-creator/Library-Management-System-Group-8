669142897f80642bc6dd831ce401e60bfa220858
Pipsy <gpflaslik7112@gmail.com>
2026-06-12 14:04:41 +0300

    chore:delete overwriting table functionality code

--- cli/Display.py
+++ cli/Display.py
@@ -47,12 +47,3 @@ def show_loans(loans):
 
 
 
-def show_books(books):
-    print("\n--- BOOKS ---")
-    for b in books:
-        print(f"{b.id} | {b.title} | {b.status}")
-
-def show_members(members):
-    print("\n--- MEMBERS ---")
-    for m in members:
-        print(f"{m.id} | {m.name} | {m.email}")
